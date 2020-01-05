7ecf9680daf9d9f74c2668cc15ed7cfcc4d4662c
gruzilla <grillen@abendstille.at>
2020-01-05 22:22:27 +0100

    advancing status...

--- collector/src/main/java/space/decarbnow/collector/rest/CustomEndpoints.java
+++ collector/src/main/java/space/decarbnow/collector/rest/CustomEndpoints.java
@@ -16,7 +16,7 @@ import space.decarbnow.collector.pojos.TwitterStatus;
  */
 @CrossOrigin(origins = "*")
 @RestController()
-@RequestMapping("**status")
+@RequestMapping("api/status")
 public class CustomEndpoints {
 
     private final PoiRepository repository;
