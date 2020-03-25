809ee580f08648cc4e05d986ebfbfb235af770d0
petres <peter.reschenhofer@gmail.com>
2020-03-25 19:04:08 +0100

    Switching to Development Mode

--- map/webpack.config.js
+++ map/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    mode: 'production',
+    mode: 'development',
     plugins: [],
     performance: {
         hints: false,
